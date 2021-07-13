6da08436b1126aa4f883ca4b83160d3628fad42e
Rıdvan Altun <ridvanaltun@outlook.com>
2021-07-13 22:33:25 +0300

    refactor: hide api variables

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"# rn-heartstone" 
+# React Native Hearthstone
+
+```bash
+# clone the project and go into it
+git clone https://github.com/ridvanaltun/rn-heartstone.git && cd rn-hearthstone
+
+# don't forget prepare your environment file
+cp .env.example .env
+
+# run android
+npm run android
+
+# run ios
+npm run ios
+```
--- src/constants/Enums.js
+++ src/constants/Enums.js
@@ -1,6 +1,11 @@
+import Config from 'react-native-config';
+
+const RAPIDAPI_KEY = Config.RAPIDAPI_KEY;
+const RAPIDAPI_HOST = Config.RAPIDAPI_HOST;
+
 export default {
   SECRETS: {
-    RAPIDAPI_KEY: 'd68ed6734dmsheb952d494c5ffc1p132b28jsn0debda5de102',
-    RAPIDAPI_HOST: 'omgvamp-hearthstone-v1.p.rapidapi.com',
+    RAPIDAPI_KEY,
+    RAPIDAPI_HOST,
   },
 };
