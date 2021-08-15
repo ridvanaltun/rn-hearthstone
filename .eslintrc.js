f5658366d5511a09d9ad6947fd92b1a9330df5a6
Rıdvan Altun <ridvanaltun@outlook.com>
2021-08-15 16:11:37 +0300

    chore: add commitlint, commitzen and prettier with lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:prettier/recommended', // resolve conflicts between prettier and eslint
+    'prettier/react',
+  ],
 };
